c3316b05d2a122c23e257d20bc28a99323198dbf
amena <amena.ruet@gmail.com>
2022-08-15 15:38:40 +0600

    prop-function

--- src/component/User.tsx
+++ src/component/User.tsx
@@ -3,13 +3,15 @@ import React, { FC } from 'react';
 interface Props{
 
     name:string,
-    age:number
+    age:number,
+    addUser: () =>void
 }
 
-const User:FC<Props> = ({name,age}) => {
+const User:FC<Props> = ({name,age,addUser}) => {
     return (
         <div>
-          <h2>hello: {name} with experience {age}</h2>  
+          <h2>hello: {name} with experience {age}</h2>
+          <button onClick={addUser}>add</button>  
         </div>
     );
 };
--- src/component/Users.tsx
+++ src/component/Users.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import User from './User';
 
+const handleAddUser=():void=>{
+    
+}
 const Users = () => {
     return (
         <div>
           <h2>users</h2>  
-          <User name='amena' age={55}></User>
+          <User name='amena' age={55} addUser={handleAddUser}></User>
         </div>
     );
 };
