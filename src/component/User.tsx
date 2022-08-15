85525254315b70b89244ca0dbc79f83567b4b4a7
amena <amena.ruet@gmail.com>
2022-08-15 16:08:48 +0600

    useEffect

--- src/component/User.tsx
+++ src/component/User.tsx
@@ -1,17 +1,22 @@
 import React, { FC } from 'react';
+import UserModel from './models';
+
+// interface Props{
+//     name:string,
+//     age:number,
+//     addUser: () =>void
+// }
 
 interface Props{
-
-    name:string,
-    age:number,
-    addUser: () =>void
+    user:UserModel,
+    addUser: (user:UserModel) =>void
 }
 
-const User:FC<Props> = ({name,age,addUser}) => {
+const User:FC<Props> = ({user,addUser}) => {
     return (
         <div>
-          <h2>hello: {name} with experience {age}</h2>
-          <button onClick={addUser}>add</button>  
+          <h2>hello: {user.name} with experience {user.email}</h2>
+          <button onClick={()=>addUser(user)}>add</button>  
         </div>
     );
 };
--- src/component/Users.tsx
+++ src/component/Users.tsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import UserModel from './models';
 import User from './User';
 
-const handleAddUser=():void=>{
-    
-}
+
 const Users = () => {
+
+    const [users,setUsers]=useState<UserModel[]>([])
+    const [team,setTeam]=useState<UserModel[]>([])
+
+    useEffect(()=>{
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res=>res.json())
+        .then(data=>setUsers(data))
+    },[])
+
+    const handleAddUser=(user:UserModel):void=>{
+        const newTeam=[...team, user]
+        setTeam(newTeam)
+        
+    }
+
     return (
         <div>
-          <h2>users</h2>  
-          <User name='amena' age={55} addUser={handleAddUser}></User>
+          <h2>users</h2>
+          <h2>Team Size: {team.length}</h2>  
+          {/* <User name='amena' age={55} addUser={handleAddUser}></User> */}
+           {
+            users.map(user=><User user={user} addUser={handleAddUser}></User>)
+           }
+        
         </div>
     );
 };
